64687597d7d4bc93067a0c75e08828812bfb77a9
Vishwanathanselvamoorthy <viswanathanselvamoorthy@gmail.com>
2024-04-06 11:12:04 +0530

    Projects Update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,7 @@
 import React from "react";
-import WorkImg from "../assets/projects/FoodExpress.png";
+import FoodExpress from "../assets/projects/FoodExpress.jpg";
+import YouTube from "../assets/projects/YouTube.jpeg";
+import NetFlix from "../assets/projects/NetFlix.jpg";
 
 const Projects = () => {
   return (
@@ -14,10 +16,54 @@ const Projects = () => {
           </p>
           <p className="py-6 font-semibold">Check out some of my recent work</p>
         </div>
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-10 ">
           <div
-            style={{ backgroundImage: `url(${WorkImg})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-lg flex justify-center items-center mx-auto content-div bg-red-600"
+            style={{ backgroundImage: `url(${FoodExpress})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-lg flex justify-center items-center mx-auto  bg-red-600 content-div"
+          >
+            <div className="opacity-0 group-hover:opacity-80">
+              <span className="text-2xl font-bold text-white tracking-wider text-center">
+                Food Express
+              </span>
+              <div className="pt-8 text-center">
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    DEMO
+                  </button>
+                </a>
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    CODE
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${YouTube})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-lg flex justify-center items-center mx-auto  bg-red-600 content-div"
+          >
+            <div className="opacity-0 group-hover:opacity-80">
+              <span className="text-2xl font-bold text-white tracking-wider text-center">
+                Food Express
+              </span>
+              <div className="pt-8 text-center">
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    DEMO
+                  </button>
+                </a>
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    CODE
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${NetFlix})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-lg flex justify-center items-center mx-auto  bg-red-600 content-div"
           >
             <div className="opacity-0 group-hover:opacity-80">
               <span className="text-2xl font-bold text-white tracking-wider text-center">
