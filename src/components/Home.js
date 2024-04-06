aeb9d077d71b5301ce302cb0197ea4854ef5bfe1
Vishwanathanselvamoorthy <viswanathanselvamoorthy@gmail.com>
2024-04-06 12:03:37 +0530

    name update

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,8 +7,8 @@ const Home = () => {
     <div name="home" className="w-full h-screen bg-[#0a192f] ">
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
         <p className="text-pink-600 font-bold">Hi,my name is</p>
-        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
-          Viswanathan
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6] tracking-widest">
+          Vishwa
         </h1>
         <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
           I'm a Front-End Developer.
@@ -16,8 +16,8 @@ const Home = () => {
         <p className="text-[#8892b0] font-semibold text-lg py-4 max-w-[700px]">
           Hello! I'm Vishwa , and I'm excited to kickstart my journey as a
           Front-End Developer. I love creating websites that look great and are
-          easy to use. I'm familiar with HTML, CSS, JavaScript, Tailwind and React, and
-          I'm eager to learn more and build awesome projects!
+          easy to use. I'm familiar with HTML, CSS, JavaScript, Tailwind and
+          React, and I'm eager to learn more and build awesome projects!
         </p>
         <div>
           <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600 rounded-lg">
