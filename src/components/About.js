c3abf310a95857f6053f3203ae9ce89a9e299c2e
Vishwanathanselvamoorthy <viswanathanselvamoorthy@gmail.com>
2024-04-06 12:30:53 +0530

    Favicon Updated

--- src/components/About.js
+++ src/components/About.js
@@ -21,11 +21,12 @@ const About = () => {
           <div>
             <p className="font-semibold ">
               I'm a computer science degree holder specializing in Front-End
-              Development. Proficient in HTML, CSS, JavaScript, Tailwind and React, I'm
-              dedicated to crafting user-friendly interfaces and delivering
-              exceptional web experiences. Constantly honing my skills and
-              staying updated with the latest design trends, I'm eager to
-              contribute my expertise to the dynamic world of web development.
+              Development. Proficient in HTML, CSS, JavaScript, Tailwind and
+              React, I'm dedicated to crafting user-friendly interfaces and
+              delivering exceptional web experiences. Constantly honing my
+              skills and staying updated with the latest design trends, I'm
+              eager to contribute my expertise to the dynamic world of web
+              development.
             </p>
           </div>
         </div>
